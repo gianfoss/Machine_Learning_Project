3cb5222f7ba4673e7f7ac9165231ad3524a9b4fa
gianfoss <gianforstevo@gmail.com>
2018-11-05 13:40:07 -0600

    update filepath

--- Test_Script.py
+++ Test_Script.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-train = 'train.csv'
+train = '/home/ssgianfortoni/ML-project/data/train.csv'
 
 corpus = CorpusGenerator(train)
 corpus.clean_data()
