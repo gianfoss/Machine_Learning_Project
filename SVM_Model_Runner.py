81235b986b3256e297da96a27d194144247aab22
gianfoss <gianforstevo@gmail.com>
2018-11-27 16:11:06 -0600

    bug fix

--- SVM_Model_Runner.py
+++ SVM_Model_Runner.py
@@ -8,13 +8,13 @@ pickle.dump(output, output_pkl)
 output_pkl.close()
 
 output = ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='lda_shrink')
-name = '/home/ssgianfortoni/ML-project/lda_output_100.pkl'
+name = '/home/ssgianfortoni/ML-project/lda_shrink_output_100.pkl'
 output_pkl = open(name, 'wb')
 pickle.dump(output, output_pkl)
 output_pkl.close()
 
 output = ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='qda')
-name = '/home/ssgianfortoni/ML-project/lda_output_100.pkl'
+name = '/home/ssgianfortoni/ML-project/qda_output_100.pkl'
 output_pkl = open(name, 'wb')
 pickle.dump(output, output_pkl)
 output_pkl.close()
