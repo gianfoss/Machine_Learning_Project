dad8604e7a2fe75be19c50e7b6355f98944a3401
gianfoss <gianforstevo@gmail.com>
2018-11-27 16:09:48 -0600

    bug fix

--- SVM_Model_Runner.py
+++ SVM_Model_Runner.py
@@ -1,7 +1,20 @@
 import ClassificationModeler
+import pickle
 
-ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='lda')
+output = ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='lda')
+name = '/home/ssgianfortoni/ML-project/lda_output_100.pkl'
+output_pkl = open(name, 'wb')
+pickle.dump(output, output_pkl)
+output_pkl.close()
 
-ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='lda_shrink')
+output = ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='lda_shrink')
+name = '/home/ssgianfortoni/ML-project/lda_output_100.pkl'
+output_pkl = open(name, 'wb')
+pickle.dump(output, output_pkl)
+output_pkl.close()
 
-ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='qda')
+output = ClassificationModeler.run_models('data/train_cleaned.csv', max_features=100, model='qda')
+name = '/home/ssgianfortoni/ML-project/lda_output_100.pkl'
+output_pkl = open(name, 'wb')
+pickle.dump(output, output_pkl)
+output_pkl.close()
