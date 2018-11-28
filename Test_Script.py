0c392b57de822f44f6dfba5b4aed6801b9508973
gianfoss <gianforstevo@gmail.com>
2018-11-28 15:16:09 -0600

    svm model

--- Test_Script.py
+++ Test_Script.py
@@ -21,7 +21,7 @@ x_train, x_test, y_train, y_test = train_test_split(data.loc[:, 'comment_text_cl
 # TF-IDF Vectors as features
 
 # word level tf-idf
-tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=1000)
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=100)
 tfidf_vect.fit(x_train)
 x_train_tfidf = tfidf_vect.transform(x_train)
 x_test_tfidf = tfidf_vect.transform(x_test)
