c14caf160fdf229d02363e21657182495ff233e2
gianfoss <gianforstevo@gmail.com>
2018-11-30 10:41:36 -0600

    change to sgd

--- Test_Script.py
+++ Test_Script.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 
 from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, f1_score, roc_auc_score
@@ -21,7 +22,7 @@ x_train, x_test, y_train, y_test = train_test_split(data.loc[:, 'comment_text_cl
 # TF-IDF Vectors as features
 
 # word level tf-idf
-tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=500)
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=1000)
 tfidf_vect.fit(x_train)
 x_train_tfidf = tfidf_vect.transform(x_train)
 x_test_tfidf = tfidf_vect.transform(x_test)
@@ -86,11 +87,11 @@ svm_predict_proba_test = []
 for i in range(6):
     # Linear SVM with grid search
 
-    param_grid_linear = {'C': np.arange(0.01, 2.01, 0.2)}
+    param_grid_SGD = {'penalty': ['l1', 'l2', 'elasticnet']}
 
-    linear_SVM_obj = SVC(kernel='linear', probability=True)
+    linear_SVM_obj = SGDClassifier(loss="modified_huber", max_iter=5)
     mod_linear_SVM = GridSearchCV(linear_SVM_obj,
-                                  param_grid_linear,
+                                  param_grid_SGD,
                                   scoring='f1',
                                   cv=5,
                                   refit=True,
